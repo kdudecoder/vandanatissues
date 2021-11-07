177bdf1104d0190acbff1ab6dcace2c847afb023
Kannan Kandappan <mail4kanna@gmail.com>
2021-11-07 08:17:19 +0530

    first commit

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$email_to = "vandanatissues@yahoo.com"; // your email address
+$email_subject = "Enquiry"; // email subject line
+$thankyou ="thankyou.php"; // thank you page
+
+// if you update the question on the form -
+// you need to update the questions answer below
+?>
+
